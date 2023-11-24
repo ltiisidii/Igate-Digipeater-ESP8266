f640bed5a07d2dc33e0e6c72942d96bba5b82253
ltiisidii <2296980+ltiisidii@users.noreply.github.com>
2023-11-24 00:46:54 +0000

    Texto en el serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,10 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial.println("Test de ESP8266");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop (){
+  
 }
