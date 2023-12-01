3c2c06e258e0eaf4bc240d02572f5f3503723db7
ltiisidii <2296980+ltiisidii@users.noreply.github.com>
2023-12-01 16:30:44 -0300

    testing aprs library LibAPRS_ESP

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 ## This is my first project with ESP8266 NodeMCU, SD1306, ArduinoTNC and the PCBoard for APRS Igate.
 
 TNC Firmware Creator: SQ9MDD
+
 PCB Creator: TA1KNN
+
+Testing APRS Library: https://github.com/williamhemmingsen/LibAPRS_ESP
 
 ![APRS Igate PCB TA1KNN](https://github.com/ltiisidii/Igate-Digipeater-ESP8266/blob/main/res/aprs-pcb.png)
---
+++ src/APRS-ESP8266-IGATE.ino
@@ -0,0 +1,173 @@
+
+/*
+ * THIS EXAMPLE IGATE SHOULD NOT BE RUN ON THE APRS-IS BEFORE
+ * IT IS COMPLETED AND BUGS ARE FIXED. IT IS CURRENTLY
+ * CORRUPTING PACKETS AND RUNNING IT MAKES THE APRS NETWORK WORSE.
+ *
+ * https://github.com/williamhemmingsen/LibAPRS_ESP/issues/3
+ */
+
+#include <ESP8266WiFi.h>
+#include <LibAPRSesp.h>
+#include <limits.h>
+#include <SimpleTimer.h>
+#include <KISS.h>
+
+const char* ssid     = "xxxxxxx";
+const char* password = "xxxxxxx";
+
+
+WiFiClient client;
+
+SimpleTimer timer;
+
+boolean KISS = false;
+
+char * lf = "\r\n";
+boolean gotPacket = false;
+char * srccall;
+char * dstcall;
+char * datapayload;
+
+AX25Msg incomingPacket;
+
+uint8_t *packetData;
+void aprs_msg_callback(struct AX25Msg *msg) {
+  if (!gotPacket) {
+    gotPacket = true;
+
+    memcpy(&incomingPacket, msg, sizeof(AX25Msg));
+
+    if (freeMemory() > msg->len) {
+      packetData = (uint8_t*)malloc(msg->len);
+      memcpy(packetData, msg->info, msg->len);
+      incomingPacket.info = packetData;
+    } else {
+      gotPacket = false;
+    }
+  }
+  
+}
+
+
+void setup() {
+  // Set up serial port
+  Serial.begin(115200);
+  ESP.wdtEnable(15000);
+
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  delay(1000);
+
+  if (!client.connect("rotate.aprs.net", 14580)) {
+    Serial.println("connection failed");
+    return;
+  } else {
+    Serial.println(F("Connected to APRS.IS"));
+    delay(1000);
+    client.print("user NOCALL pass YOURCODE vers YOUR VERSION \r\n");\\ Your CAll, APRS.IS code, and version comment
+  }
+
+  APRS_init();
+  APRS_setCallsign("NOCALL", 1); ///Your call
+  APRS_setPath1("WIDE1", 1);
+  APRS_setPath2("WIDE2", 2);
+  APRS_setPreamble(450);
+  APRS_setTail(150);
+}
+
+
+void processPacket() {
+  boolean tcpippacket = false;
+  if (gotPacket) {
+    gotPacket = false;
+    if (KISS) {
+      kiss_wrapper();
+    }
+    else {
+      srccall = incomingPacket.src.call;
+      dstcall = incomingPacket.dst.call;
+      char * datastr = (char*)incomingPacket.info;
+      datastr[incomingPacket.len] = '\0';
+      //Fix for digirepeated TCPIP APRS.IS Packets
+      if (datastr[0] == '}') {
+        tcpippacket = true;
+      }
+      datapayload = datastr;
+      char sssid[10];
+      itoa (incomingPacket.src.ssid, sssid, 10);
+      char dssid[10];
+      itoa (incomingPacket.dst.ssid, dssid, 10);
+      char packetchar[200];
+      packetchar[0] = 0;
+      strcat (packetchar, srccall);
+      strcat (packetchar, "-");
+      strcat (packetchar, sssid);
+      strcat (packetchar, ">");
+      strcat (packetchar, dstcall);
+      strcat (packetchar, "-");
+      strcat (packetchar, dssid);
+      for (int i = 0; i < incomingPacket.rpt_count; i++) {
+        strcat (packetchar, ",");
+        char * rptcall = incomingPacket.rpt_list[i].call;
+        strcat (packetchar, rptcall);
+        strcat (packetchar, "-");
+        char rssid[10];
+        itoa (incomingPacket.rpt_list[i].ssid, rssid, 10);
+        strcat (packetchar, rssid);
+      }
+      strcat (packetchar, ",qAR,NOCALL-1:"); /// Your call and SSID
+      /* BUG #3 here: string function stops copying on the first NUL byte */
+      strcat (packetchar, datapayload);
+      //strcat (packetchar, lf);
+
+      /* BUG #3: println stops writing on the first NUL byte */
+      Serial.println(packetchar);
+
+      //Don't IGATE TCPIP repeaded packets
+      if (0 && !tcpippacket) {
+        /* BUG #3 present here too: println stops writing on the first
+         * NUL byte. Do not run this before fixing and testing on packets
+         * with NUL bytes. They exist and this code corrupts the packets
+         * further.
+         */
+        //client.println(packetchar);
+        //client.println();
+      }
+    }
+    free(packetData);
+
+  }
+}
+
+boolean whichExample = false;
+void loop() {
+
+
+  if (Serial.available() && KISS) {      // If anything comes in Serial (USB),
+    kiss_serial(Serial.read());
+  }
+
+  if (client.available()) {     // If anything comes from APRS.IS
+    Serial.write(client.read());   // read it and send it out Serial (USB)
+  }
+  processPacket();
+  ESP.wdtFeed();
+}
+
+
